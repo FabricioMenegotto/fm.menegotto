757ddaa165250c000d6268bc420f86f6f56ecdc2
FabricioMenegotto <fabriciofmenegotto@gmail.com>
2025-05-26 19:43:48 -0300

    fix: evtar duplicaçao de ID

--- src/main/java/models/Usuario.java
+++ src/main/java/models/Usuario.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Usuario {
 
    @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private String email;
